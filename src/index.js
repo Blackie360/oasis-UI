ffd2bb111644daaab9c7b55952fb0a3712d228a7
Felix Jumason <felixkent360@gmail.com>
2023-09-15 16:09:22 +0300

    browser icon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, <Analytics   mode="production"/>, document.getElementById('root'));
+ReactDOM.render(<App />,  document.getElementById('root'));
